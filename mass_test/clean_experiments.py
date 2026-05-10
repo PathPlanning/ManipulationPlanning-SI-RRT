58c9f3047c3f0304ad078e64bcf0e9b76db7f685
kaizer-nurik <kerimovnm@mail.ru>
2026-05-10 03:23:56 +0000

    fix clean_exps (clean_exps was from multiagent somehow)

--- mass_test/clean_experiments.py
+++ mass_test/clean_experiments.py
@@ -1,29 +1,45 @@
 """
+Clean planning results and visualisation videos and .blend files from mass test.
+
 python3 ./mass_test/clean_experiments.py
 
+To delete visualisation set delete_videos = True
+
+To delete logs set delete_logs = True
+
+Set test directories at test_dirs
 """
 
 import argparse
 import os
-import shutil
 
+delete_videos = False
+delete_logs = False
+
+test_dirs = [f"./mass_test/tests/{i}_spheres" for i in range(0,301,20)]
+test_dirs[0] ="./mass_test/tests/1_spheres"
 
 def main() -> None:
-
-
-    
-    path_to_tasks = './multiagent_tests'
-    tasks_by_robot_count = os.listdir(path_to_tasks)
-        
     futures = []
-    for robot_count in tasks_by_robot_count:
-        test_cases = os.listdir(os.path.join(path_to_tasks,robot_count))
-        for test_case in test_cases:
-            for i in range(10):
-                test_dir_path = os.path.join(path_to_tasks,robot_count,test_case,"generated_tasks_msirrt_seed_0")
-                if os.path.exists(test_dir_path):
-                    print(test_dir_path)
-                    shutil.rmtree(test_dir_path)
+    for test_dir in test_dirs:
+        test_numbers = os.listdir(test_dir)
+        for test_number_dir in test_numbers:
+            files = os.listdir(os.path.join(test_dir,test_number_dir))
+            for file in files:
+                if delete_videos:
+                    if '.mp4' in file:
+                        print(os.path.join(test_dir,test_number_dir,file))
+                        os.remove(os.path.join(test_dir,test_number_dir,file))
+                    elif 'json.blend' in file:#,"mass_test.blend"]:
+                        print(os.path.join(test_dir,test_number_dir,file))
+                        os.remove(os.path.join(test_dir,test_number_dir,file))
+                if delete_logs:
+                    if ('MSIRRT_planner_logs' in file or 'DRGBT_planner_logs' in file or 'STRRT*_planner_logs' in file) and (file[-5:]=='.json'):
+                        print(os.path.join(test_dir,test_number_dir,file))
+                        os.remove(os.path.join(test_dir,test_number_dir,file))
+    
+    for future in futures:
+        future.result()
         
 if __name__ == '__main__':
     main()
